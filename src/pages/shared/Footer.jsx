18f45ec85447362c283c7d98f8ea92a70793f923
ajjaj ahmed <ajjajahmedasif@gmail.com>
2024-12-30 14:01:48 +0600

    adding banner section

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png'
 
 const Footer = () => {
     return (
-        <footer className="footer bg-base-200 text-base-content p-10">
+        <footer className="footer bg-base-200 rounded-xl text-base-content p-10">
             <aside>
                 <img className='w-16' src={logo} alt="" />
                 <p>
