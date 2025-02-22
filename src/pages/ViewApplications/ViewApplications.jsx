ba4588963e2e305958a54f5a4e84bef6fb311aae
ajjaj ahmed <ajjajahmedasif@gmail.com>
2025-02-22 09:43:13 +0600

    adding status function

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ViewApplications = () => {
     const applications = useLoaderData();
+    const handleStatusUpdate = (e, id) => {
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.modifiedCount){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Status has been updated.",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+                
+            })
+    }
     return (
         <div className='my-4 rounded-xl'>
             <div className="overflow-x-auto">
@@ -17,17 +43,27 @@ const ViewApplications = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        
-                     {
-                        applications.map((application, index)=> <tr key={index} className="bg-base-200">
-                            <th>{index + 1}</th>
-                            <td>{application.applicant_email}</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        )
-                     }
-                      
+
+                        {
+                            applications.map((app, index) => <tr key={app._id} className="bg-base-200">
+                                <th>{index + 1}</th>
+                                <td>{app.applicant_email}</td>
+                                <td>Quality Control Specialist</td>
+                                <td>
+                                    <select
+                                        onChange={(e) => handleStatusUpdate(e, app._id)}
+                                        defaultValue={app.status || 'Change Status'} className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled>Change Status</option>
+                                        <option>Under Review</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            )
+                        }
+
                     </tbody>
                 </table>
             </div>
